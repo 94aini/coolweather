87bd00e693bd12b6e1c9bf21310cbce8f5d3ccf6
mateng <1933915243@qq.com>
2018-11-23 11:59:43 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/mateng/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/mateng/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.mateng.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.mateng.coolweather.gson.Forecast;
 import com.example.mateng.coolweather.gson.Weather;
+import com.example.mateng.coolweather.service.AutoUpdateService;
 import com.example.mateng.coolweather.util.HttpUtil;
 import com.example.mateng.coolweather.util.Utility;
 
@@ -158,6 +160,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         }else {
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
